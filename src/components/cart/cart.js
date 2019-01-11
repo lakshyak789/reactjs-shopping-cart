e34e231f94d716b7222b85eae5a1cbe4837f404c
Lakshya Khanna <rahul.sharma@actionable-science.com>
2019-01-11 11:19:38 +0530

    certain fixes and design changes

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -1,36 +1,60 @@
-import React from 'react';
-import './cart.css';
-import data from '../../data.json';
-const Cart = (props) =>
-{ 
-    let lid = 0;
-    let sum= 0;
-    
-    const listitems = data.items.map( data => (
-        props.array.includes(data.id) ? 
-            <frameElement>
-                <div className="hidden"> {sum = data.cost +sum} </div> 
-                <div className="item-id"> <h4>{lid= lid+1 }</h4>  </div> 
-                <div className="itemname"> <h4>{data.name} </h4> </div> 
-                <div className="itemcost"><h4> {data.cost}</h4></div>
-                <div className="cart-image-wrap"> <img src={data.imageurl} alt="item"></img> </div> 
-           </frameElement>  :  null
-    ))
-    return(
-        <div className="cart-main">
-            <h2>User Cart</h2> 
-            <div className="item-wrap">
-            <div className="item-id"> <h4>Item Number</h4> </div>
-            <div className="itemname"> <h4>Item Name</h4> </div>
-            <div className="itemcost"> <h4>Item Cost </h4></div>
-            <div className="cart-image-wrap"><h4>Image</h4></div>
-            {listitems}
-            </div>
-            <div className="total">
-            <h2>Total: {sum}</h2>
-            </div>
+import React from "react";
+import "./cart.css";
+import data from "../../data.json";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+const Cart = props => {
+  let lid = 0;
+  let sum = 0;
+
+  const listitems = data.items.map(data =>
+    props.array.includes(data.id) ? (
+      <frameElement>
+        <div className="hidden"> {(sum = data.cost + sum)} </div>
+        <div className="item-id">
+          {" "}
+          <h4>{(lid = lid + 1)}</h4>{" "}
         </div>
-    )
-}
+        <div className="itemname">
+          {" "}
+          <h4>{data.name} </h4>{" "}
+        </div>
+        <div className="itemcost">
+          <h4> {data.cost}</h4>
+        </div>
+        <div className="cart-image-wrap">
+          {" "}
+          <img src={data.imageurl} alt="item" />{" "}
+        </div>
+      </frameElement>
+    ) : null
+  );
+  return (
+    <div className="cart-main">
+      <h2>User Cart</h2>
+      <div className="item-wrap">
+        <div className="item-id">
+          {" "}
+          <h4>Item Number</h4>{" "}
+        </div>
+        <div className="itemname">
+          {" "}
+          <h4>Item Name</h4>{" "}
+        </div>
+        <div className="itemcost">
+          {" "}
+          <h4>Item Cost </h4>
+        </div>
+        <div className="cart-image-wrap">
+          <h4>Image</h4>
+        </div>
+        {listitems}
+      </div>
+      <div className="total">
+        <h2>Total: {sum}</h2>
+      </div>
+    </div>
+  );
+};
 
 export default Cart;
--- src/components/shopping/shopping.js
+++ src/components/shopping/shopping.js
@@ -1,111 +1,136 @@
-import React,{Component} from 'react';
-import data from '../../data.json';
-import './shopping.css';
-import Cart from '../cart/cart';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import data from "../../data.json";
+import "./shopping.css";
+import Cart from "../cart/cart";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
 class Shopping extends Component {
-    state ={
-        cart: [],
-        cartstatus: false,
-        filtersize: []
+  state = {
+    cart: [],
+    cartstatus: false,
+    filtersize: []
+  };
+  addtocart(id) {
+    if (this.state.cart.includes(id)) {
+      alert("item already there");
+    } else {
+      this.setState(prevstate => ({ cart: [...prevstate.cart, id] }));
+      if (localStorage.getItem("items") === null) {
+        let item = this.state.cart;
+        localStorage.setItem("items", JSON.stringify(item));
+      } else {
+        let items = JSON.parse(localStorage.getItem("items"));
+        items.push(id);
+        localStorage.setItem("items", JSON.stringify(items));
+      }
     }
-    addtocart(id){
-      if(this.state.cart.includes(id) )  {
-        alert("item already there") ;
-      } 
-      else {
-        this.setState(prevstate => ({ cart: [...prevstate.cart, id] }));
-        if(localStorage.getItem("items") === null)
-        {   
-            let item = this.state.cart;
-            localStorage.setItem("items", JSON.stringify(item));
-        }
-        else {
-            let items = JSON.parse(localStorage.getItem('items'));
-            items.push(id);
-            localStorage.setItem("items", JSON.stringify(items));
-        }
-      } 
-        this.setState({cartstatus: true});
+    this.setState({ cartstatus: true });
+  }
+  handleCart() {
+    const assign = this.state.cartstatus ? false : true;
+    this.setState({ cartstatus: assign });
+  }
+  filtersizes(e) {
+    if (this.state.filtersize.includes(e.target.id)) {
+      let index = this.state.filtersize.indexOf(e.target.id);
+      let arr = [...this.state.filtersize];
+      arr.splice(index, 1);
+      this.setState({ filtersize: arr });
+    } else {
+      this.setState({ filtersize: [...this.state.filtersize, e.target.id] });
+      console.log(this.state.filtersize);
     }
-    handleCart(){
-        const assign = this.state.cartstatus ? false : true;
-        this.setState({cartstatus: assign});
-    }
-    filtersizes(e){
-        if(this.state.filtersize.includes(e.target.id))
-        {
-            let index = this.state.filtersize.indexOf(e.target.id);
-            let arr = [...this.state.filtersize];
-            arr.splice(index , 1);
-            this.setState({filtersize: arr})
-         }
-        else {
-        this.setState({filtersize:[...this.state.filtersize , e.target.id]});
-        console.log(this.state.filtersize);
-        }
-    }
-    render(){
-        const dataa =  data.items.map(data => {
-            if(this.state.filtersize.length <= 0){
-                return (
-                    <div className ="four-col" key ={data.id}>
-                <div className ="image-wrap">
-                <Link to = {{pathname: `/${data.id}`} } >
-                  <img src = {data.imageurl} alt="shopping" />
-               </Link>
-                </div> 
-                <div className="name-wrap">
-                  <h3> {data.name}</h3> 
-                  <h4> Cost: {data.cost} </h4>
-                  <button className="add-cart" onClick ={() => this.addtocart(data.id)}> Add To Cart </button>
-                </div>
-                </div>
-                    )
-            }
-            else {
-                return (
-                    this.state.filtersize.includes(data.size) ?
-                    <div className ="four-col" key ={data.id}>
-             
-               <div className ="image-wrap">
-                 
-               <Link to = {{pathname: `/${data.id}`} } >
-                  <img src = {data.imageurl} alt="shopping" />
-               </Link>
-                </div> 
-                <div className="name-wrap">
-                  <h3> {data.name}</h3> 
-                  <h4> Cost: {data.cost} </h4>
-                  <button className="add-cart" onClick ={() => this.addtocart(data.id)}> Add To Cart </button>
-                </div>
-                </div> : null
-                    )
-            }
-        }    
-    ); 
-        return(
-            <React.Fragment>
-            <div className="container">
-            <div className="filter-sizes">
-                <div className={this.state.filtersize.includes("xl") ? "size active" : "size"} id="xl" onClick={this.filtersizes.bind(this)}>  xl </div>
-                <div className={this.state.filtersize.includes("l") ? "size active" : "size"} id="l" onClick={this.filtersizes.bind(this)}>  l </div>
-                <div className={this.state.filtersize.includes("m") ? "size active" : "size"} id="m" onClick={this.filtersizes.bind(this)}> m </div>
+  }
+  render() {
+    const dataa = data.items.map(data => {
+      if (this.state.filtersize.length <= 0) {
+        return (
+          <div className="four-col" key={data.id}>
+            <div className="image-wrap">
+              <Link to={{ pathname: `/${data.id}` }}>
+                <img src={data.imageurl} alt="shopping" />
+              </Link>
             </div>
-            <h2 className="heading">React Shopping Cart Example</h2>
-            <div className="cart-button" onClick = {this.handleCart.bind(this)}>
-                {this.state.cartstatus ?  "Close Cart"  : "Go To cart"}
+            <div className="name-wrap">
+              <h3> {data.name}</h3>
+              <h4> Cost: {data.cost} </h4>
+              {/* <button className="add-cart" onClick={() => this.addtocart(data.id)}>
+                Add To Cart
+              </button> */}
+              <Button onClick={() => this.addtocart(data.id, true)}>
+                Add to Cart
+              </Button>
             </div>
-                   {dataa}
-                   
+          </div>
+        );
+      } else {
+        return this.state.filtersize.includes(data.size) ? (
+          <div className="four-col" key={data.id}>
+            <div className="image-wrap">
+              <Link to={{ pathname: `/${data.id}` }}>
+                <img src={data.imageurl} alt="shopping" />
+              </Link>
             </div>
-            {this.state.cartstatus ?
-            <Cart array={this.state.cart}/> 
-            : null
-            }
-            
-            </React.Fragment>
-        )
-    }
+            <div className="name-wrap">
+              <h3> {data.name}</h3>
+              <h4> Cost: {data.cost} </h4>
+              <button
+                className="add-cart"
+                onClick={() => this.addtocart(data.id)}
+              >
+                {" "}
+                Add To Cart{" "}
+              </button>
+            </div>
+          </div>
+        ) : null;
+      }
+    });
+    return (
+      <React.Fragment>
+        <div className="container">
+          <div className="filter-sizes">
+            <div
+              className={
+                this.state.filtersize.includes("xl") ? "size active" : "size"
+              }
+              id="xl"
+              onClick={this.filtersizes.bind(this)}
+            >
+              {" "}
+              xl{" "}
+            </div>
+            <div
+              className={
+                this.state.filtersize.includes("l") ? "size active" : "size"
+              }
+              id="l"
+              onClick={this.filtersizes.bind(this)}
+            >
+              {" "}
+              l{" "}
+            </div>
+            <div
+              className={
+                this.state.filtersize.includes("m") ? "size active" : "size"
+              }
+              id="m"
+              onClick={this.filtersizes.bind(this)}
+            >
+              {" "}
+              m{" "}
+            </div>
+          </div>
+          <h2 className="heading">React Shopping Cart Example</h2>
+          <div className="cart-button" onClick={this.handleCart.bind(this)}>
+            {this.state.cartstatus ? "Close Cart" : "Go To cart"}
+          </div>
+          {dataa}
+        </div>
+        {this.state.cartstatus ? <Cart array={this.state.cart} /> : null}
+      </React.Fragment>
+    );
+  }
 }
 export default Shopping;
